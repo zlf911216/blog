2f031cd6ebbe135c70e425fe470baaa59984fe7f
zlf <zlf911216@sina.com>
2016-10-31 15:56:25 +0800

    zlf

--- src/components/article-write/index.vue
+++ src/components/article-write/index.vue
@@ -12,22 +12,52 @@
 			<div class="title-name" flex-box="0">说明:</div>
 			<input type="text" flex-box="1">
 		</div>
+		<div id="editor" v-html="inputContent" @input="outputContent"></div>
+
 	</div>		
 </template>
 <script>
+import WangEditor from 'wangeditor'
 	export default {
 		name:"artWrite",
 		data(){
 			return{
-				
+				content: '',
+				inputContent:'',
+				uploadUrl:'/upload'
 			}
+		},
+		methods: {
+	        createEditor() {
+	            const self = this
+	            const editor = new WangEditor('editor')
+	            editor.config.menus = ['source', '|', 'bold', 'underline', 'italic', 'strikethrough', 'eraser', 'forecolor', 'bgcolor', '|', 'quote', 'fontfamily', 'fontsize', 'head', 'unorderlist', 'orderlist', 'alignleft', 'aligncenter', 'alignright',
+	                '|', 'link', 'unlink', 'table', 'img', 'insertcode', '|', 'undo', 'redo', 'fullscreen'
+	            ]
+	            editor.config.uploadImgUrl = this.uploadUrl
+	            editor.onchange = function() {
+	                self.formatContent(this.$txt.html())
+	            }
+	            editor.create()
+	        },
+	        formatContent(content) {
+	            this.content = content
+	            this.outputContent()
+	        },
+	        outputContent() {
+	            this.$emit('input', this.content)
+	        }
+	    },
+		mounted:function(){
+			this.createEditor()
 		}
 	}
 </script>
 <style scoped>
 	.write{
-		max-width: 600px;
 		margin:auto;
+		border-left:1px solid rgba(0,0,0,0.15);
+		border-right:1px solid rgba(0,0,0,0.15);
 	}
 	input{
 		margin-left: 4%;
@@ -42,4 +72,12 @@
 		padding: 0 6% 0 3%;
 		border-bottom:1px solid rgba(0,0,0,0.15);
 	}
+	#editor{
+        height: 500px;
+    }
+    .wangEditor-container{
+        border-radius: 2px;
+        overflow: hidden;
+        border: 1px solid #CCC;
+    }
 </style>
